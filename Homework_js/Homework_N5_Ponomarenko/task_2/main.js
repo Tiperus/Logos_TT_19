f187e5df4ef8065fd10419c87b22df7b0ee1d38e
Taras <63805334+Tiperus@users.noreply.github.com>
2020-05-05 09:09:45 +0300

    Add files via upload

--- Homework_js/Homework_N5_Ponomarenko/task_2/main.js
+++ Homework_js/Homework_N5_Ponomarenko/task_2/main.js
@@ -1,12 +1,30 @@
 let num=+prompt("Please put number")
-result(num)
+let x=num;
+// result(num)
 function result(a){
-    let i;
+    
+    let rangeSimple =[];
     next:
-    for(i=1; i<=a; i++){
+    for(let i=1; i<=a; i++){
         for (let j = 2; j < i; j++){
         if(i%j==0) continue next;}
-        console.log(i)
+        // console.log(i);
+        rangeSimple[i] =i;
+
     }
-    
+    return rangeSimple;
 }
+let y=(result(num));
+// let argum = function filter(y,x) {
+//     if (x==y)return y;
+//     else return(0);
+//   };
+let korectNumber = y.filter(function(number) {
+    return number == x;
+  });
+  if (korectNumber.length==0) alert("Число складне -"+x);
+    else alert("Число просте -"+x);
+//   console.log(x)  
+//   if (argum==empty) alert("Число складне");
+//   else{alert("Число просте");  
+// console.log(argum);}
