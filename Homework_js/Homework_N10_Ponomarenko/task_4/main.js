527928070f12ec0f40cd4c18d6de9f4a3cc7c0c6
Taras <63805334+Tiperus@users.noreply.github.com>
2020-05-11 11:42:22 +0300

    Add files via upload

--- Homework_js/Homework_N10_Ponomarenko/task_4/main.js
+++ Homework_js/Homework_N10_Ponomarenko/task_4/main.js
@@ -1,7 +1,26 @@
-const arr=["html", "css", "js", "html", "js", "python", "js", "scss"];
-
-const newArr = [ ... new Set ( arr ) ] ;
 
 
-console.log(newArr);
+const arr=["html","css","js","html","js","python","js","scss"];
+let newArr=[]
+for(let i=0; i<arr.length; i++){
+    for(let b=0; b<arr.length; b++){
+    if (arr[i]===arr[b] && i!=b)arr.splice(b,1);
+    else newArr[i]=arr[i];
+    }
+    
+}
+console.log(arr);
+console.log(newArr)
+
+
+// Other possibility
+
+// const arr=["html", "css", "js", "html", "js", "python", "js", "scss"];
+
+// const newArr = [ ... new Set ( arr ) ] ;
+
+
+// console.log(newArr);
+
+// _____________________
 
